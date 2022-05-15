3e6fae5e25ae7b4185330fa9399e8282c3bab0dd
Sourabh Ruhella <sourabh.ruhella3108@gmail.com>
2022-05-15 21:18:01 +0530

    added procfile

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const Emitter = require('events')
 // Database connection
 mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
 const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log('Database connected...');
-// }).catch(err => {
-//     console.log('Connection failed...')
-// });
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
 
 
 // Session store
